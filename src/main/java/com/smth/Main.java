edbe204c468f0187fd586b101b5962f639455ae0
sorina-vasiliu <vasiliu.sorina@gmail.com>
2016-07-12 13:16:13 +0300

    Minor changes to build script

--- src/main/java/com/smth/Main.java
+++ src/main/java/com/smth/Main.java
@@ -1,12 +1,14 @@
 package com.smth;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by Dragos on 7/12/2016.
  */
 public class Main {
 
     public static void main(String args[]){
-        System.out.println("X");
+        System.out.println(StringUtils.reverse(args[0]));
     }
 
 
