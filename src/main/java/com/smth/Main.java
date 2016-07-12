d03fce0f193bb68d12f6ae069add5ce646968c07
sorina-vasiliu <vasiliu.sorina@gmail.com>
2016-07-12 12:13:07 +0300

    Initial commit of the gradle study project

---
+++ src/main/java/com/smth/Main.java
@@ -0,0 +1,13 @@
+package com.smth;
+
+/**
+ * Created by Dragos on 7/12/2016.
+ */
+public class Main {
+
+    public static void main(String args[]){
+        System.out.println("X");
+    }
+
+
+}
